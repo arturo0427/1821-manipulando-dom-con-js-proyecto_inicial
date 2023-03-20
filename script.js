aa2c9e5d4ff535ca7418b0efdfc7a3324c148cd9
amunoz <amunoz@PASAN-TI.VFE.EC>
2023-03-20 13:16:06 -0500

    Clase04 completada

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 
-const btnAgregar = document.querySelector("[data-form-btn]");
+
+    const btnAgregar = document.querySelector("[data-form-btn]");
 
 
-const createTask = (e) => {
-    e.preventDefault();
-    const inputTask = document.querySelector("[data-form-input]");
-    const valueInput = inputTask.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement('li');
-    task.classList.add("card");
-    const content = `
-    <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${valueInput}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    const createTask = (e) => {
+        e.preventDefault();
+        const inputTask = document.querySelector("[data-form-input]");
+        const valueInput = inputTask.value;
+        const list = document.querySelector("[data-list]");
+        const task = document.createElement('li');
+        task.classList.add("card");
 
-    task.innerHTML = content;
-    list.appendChild(task)
-    console.log(content);
-    inputTask.value = "";
-};
-btnAgregar.addEventListener("click", createTask);
+        const taskContent = document.createElement("div");
+        taskContent.appendChild(checkComplete());
+
+        const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = valueInput;
+        taskContent.appendChild(titleTask);
+
+        const i = document.createElement("i");
+        i.classList.add("fas", "fa-trash-alt", "trashIcon");
+        taskContent.appendChild(i);
+
+
+
+        task.appendChild(taskContent)
+        list.appendChild(task)
+
+        inputTask.value = "";
+    };
+    btnAgregar.addEventListener("click", createTask);
+
+    const checkComplete = () => {
+        const i = document.createElement("i");
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+
+        return i;
+    };
+
+
+    const completeTask = (e) => {
+        const element = e.target;
+        element.classList.toggle("fas");
+        element.classList.toggle("completeIcon");
+        element.classList.toggle("far");
+
+    };
+
