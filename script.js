966f185fe0d0acad4ada7698c359e125a89b389a
amunoz <amunoz@PASAN-TI.VFE.EC>
2023-03-20 12:30:01 -0500

    Clase 03 completada

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const btnAgregar = document.querySelector("[data-form-btn]");
 
 const createTask = (e) => {
     e.preventDefault();
-    const inputTare = document.querySelector("[data-form-input]");
-    console.log(inputTare)
+    const inputTask = document.querySelector("[data-form-input]");
+    const valueInput = inputTask.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement('li');
+    task.classList.add("card");
+    const content = `
+    <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${valueInput}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+    task.innerHTML = content;
+    list.appendChild(task)
+    console.log(content);
+    inputTask.value = "";
 };
 btnAgregar.addEventListener("click", createTask);
